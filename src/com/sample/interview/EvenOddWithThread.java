66b43ba85a6f14ba293246f411c5a1c26be3ed8c
Sushant Kadam <sushantkadam02@gmail.com>
2026-07-10 23:28:43 +0530

    Thread even odd number practice

---
+++ src/com/sample/interview/EvenOddWithThread.java
@@ -0,0 +1,57 @@
+package com.sample.interview;
+
+public class EvenOddWithThread {
+
+    private final int limit;
+    private int counter;
+
+    EvenOddWithThread(int limit) {
+        this.limit = limit;
+    }
+
+    public static void main(String[] args) {
+        EvenOddWithThread printer = new EvenOddWithThread(10);
+
+        Thread t1 = new Thread(printer::oddNumber, "oddThread");
+        Thread t2 = new Thread(printer::evenNumber, "evenThread");
+
+        t1.start();
+        t2.start();
+    }
+
+
+
+    private synchronized void evenNumber() {
+        while (counter <= limit) {
+            while (counter % 2 != 0) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (counter <= limit) {
+                System.out.print(counter+" ");
+                counter++;
+                notifyAll();
+            }
+        }
+    }
+
+    private synchronized void oddNumber() {
+        while (counter <= limit) {
+            while (counter % 2 == 0) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (counter <= limit) {
+                System.out.print(counter++);
+                counter++;
+                notifyAll();
+            }
+        }
+    }
+}
