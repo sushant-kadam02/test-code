15931bcc5b8c905e602c73244c936d5946e5a4d7
Sushant Kadam <sushantkadam02@gmail.com>
2026-07-10 23:43:54 +0530

    Frequency count with Map

---
+++ src/com/sample/interview/FrequencyCounter.java
@@ -0,0 +1,20 @@
+package com.sample.interview;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FrequencyCounter {
+    public static void main(String[] args) {
+        int arr[] = {2,3,4,5,6,6,7,4,1,7,8,9,0,5,6,8,9,0,1};
+
+        Map<Integer, Integer> frqMap = new HashMap<>();
+
+        for (int num : arr) {
+            frqMap.put(num, frqMap.getOrDefault(num, 0)+1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : frqMap.entrySet()) {
+            System.out.println("Element : "+entry.getKey() +" Count : "+entry.getValue());
+        }
+    }
+}
